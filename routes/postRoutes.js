d8e4a633e743a8ffa5126805006503f15c22f4f3
Rakesh <iamrakeshkumar@pm.me>
2021-08-31 20:43:03 +0530

    removing id for protect

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -18,9 +18,11 @@ import { protect } from "../middlewares/authMiddleWare.js";
 router.route("/");
 router.param("postId", findPostById);
 
-router.route("/").get(getAllPosts);
-
-router.route("/:_id").post(protect, addPost).delete(protect, removePost);
+router
+  .route("/")
+  .get(getAllPosts)
+  .post(protect, addPost)
+  .delete(protect, removePost);
 
 router
   .route("/:postId/like")
