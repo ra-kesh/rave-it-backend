ef604f86e398e2bd72c0e7e975e5f5db8bcf4f73
Rakesh <iamrakeshkumar@pm.me>
2021-09-04 09:34:51 +0530

    fix spelling

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import connectionRoutes from "./routes/connectionRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
 
 import {
@@ -23,31 +23,33 @@ connectDB();
 //connecting main app
 const app = express();
 app.use(cors());
+
 // cors
-let allowedOrigins = ["http://localhost:3000", "http://192.168.43.156:3000"];
-app.use(function (req, res, next) {
-  var origin = req.headers.origin;
-  console.log("origin: ", origin);
+// let allowedOrigins = ["http://localhost:3000", "http://192.168.43.156:3000"];
 
-  if (origin) {
-    if (allowedOrigins.indexOf(origin) > -1) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-    } else {
-      console.log("trying to access from other origin.");
-      return res.status(400).json({ message: "Origin not allowed" });
-    }
-  }
+// app.use(function (req, res, next) {
+//   var origin = req.headers.origin;
+//   console.log("origin: ", origin);
 
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+//   if (origin) {
+//     if (allowedOrigins.indexOf(origin) > -1) {
+//       res.setHeader("Access-Control-Allow-Origin", origin);
+//     } else {
+//       console.log("trying to access from other origin.");
+//       return res.status(400).json({ message: "Origin not allowed" });
+//     }
+//   }
+
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 // middlewares
 app.use(morgan("dev"));
@@ -62,7 +64,7 @@ app.get("/", (req, res) => {
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 app.use("/notifications", notificationRoutes);
-app.use("/connection", connectionRouter);
+app.use("/connection", connectionRoutes);
 
 // error Handellers
 app.use(notFoundMiddleware);
